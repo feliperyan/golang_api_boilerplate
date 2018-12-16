95f23d9f0b78bb1a47a3e47f6dc396dc653417d7
Felipe Ryan <fryan@fryan-ltm3.internal.salesforce.com>
2018-12-16 23:30:16 +0100

    port

--- server.go
+++ server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -11,12 +13,14 @@ import (
 // our main function
 func main() {
 
+	port := os.Getenv("PORT")
+
 	router := mux.NewRouter()
 	router.Use(JwtAuthentication)
 
 	router.HandleFunc("/api/user/new", CreateAccount).Methods("POST")
 	router.HandleFunc("/api/user/login", Authenticate).Methods("POST")
 	router.HandleFunc("/api/dummy", DummyResponse).Methods("GET")
-
-	log.Fatal(http.ListenAndServe(":8000", router))
+	p := fmt.Sprintf(":%v", port)
+	log.Fatal(http.ListenAndServe(p, router))
 }
