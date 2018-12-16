f07f15a6e2b726702714de06844cc4bef01bdc7b
Felipe Ryan <fryan@fryan-ltm3.internal.salesforce.com>
2018-12-16 22:55:17 +0100

    taking care of heroku postgresql

--- models.go
+++ models.go
@@ -29,6 +29,10 @@ func init() {
 	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
 	fmt.Println(dbUri)
 
+	if os.Getenv("DATABASE_URL") != "" {
+		dbUri = os.Getenv("DATABASE_URL")
+	}
+
 	conn, err := gorm.Open("postgres", dbUri)
 	if err != nil {
 		fmt.Print(err)
