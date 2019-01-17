046ee1de0895c38a48cb4106894a08a0ae314a93
Felipe Ryan <fryan@fryan-ltm3.internal.salesforce.com>
2019-01-17 23:12:11 +0100

    new feature

--- models.go
+++ models.go
@@ -164,5 +164,7 @@ func GetRandomQuote() string {
 
 	rand.Seed(time.Now().UnixNano())
 	num := rand.Intn(len(*quotes))
-	return (*quotes)[num] // stupid syntax, more info: https://flaviocopes.com/golang-does-not-support-indexing/
+	s := (*quotes)[num] // stupid syntax, more info: https://flaviocopes.com/golang-does-not-support-indexing/
+	s = fmt.Sprintf("Albert Einstein Once said: %s", s)
+	return s
 }
